638201e4bafa6858e29be0892745d9acc5042887
enicxus <tineoanderson7@gmail.com>
2023-04-02 22:36:37 -0400

    5.0

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+
+
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const continuarButton = document.querySelector('.sign-in-container button');
+
+continuarButton.addEventListener('click', function(event) {
+  if (emailInput.value.trim() !== '' && passwordInput.value.trim() !== '') {
+    window.location.href = 'entorno.html';
+  } else {
+    event.preventDefault();
+  }
+});
