0027e853a626ca55027b1085b0675ce2224984f0
enicxus <tineoanderson7@gmail.com>
2023-05-01 14:52:17 -0400

    restricion_gri

--- vali_index.js
+++ vali_index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    // Bloquear el uso de la tecla espacio en todos los inputs
-    $('input').on('keydown', function (e) {
-        if (e.keyCode === 32) {
+    // Bloquear el uso de la tecla espacio y letra ñ en todos los inputs de tipo texto y email
+    $('input[type="text"], input[type="email"]').on('keydown', function (e) {
+        if (e.keyCode === 32 || e.key === "ñ" || e.key === "Ñ") {
             return false;
         }
     });
@@ -26,8 +26,6 @@ $(document).ready(function () {
         }
     });
 
-
-
     // Función para validar el correo electrónico
     function isValidEmail(email) {
         var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
@@ -37,6 +35,7 @@ $(document).ready(function () {
         return emailRegex.test(email) &&
             localPart.length <= 64 &&
             domainPart.length >= 4 && domainPart.length <= 255 &&
-            email.length <= 256;
+            email.length <= 256 &&
+            !/ñ/.test(email) && !/Ñ/.test(email) && !/ñ/.test(domainPart) && !/Ñ/.test(domainPart);
     }
 });
