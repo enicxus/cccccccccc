29f606d3139c1df47bc50f35c3cd2804973e8570
enicxus <tineoanderson7@gmail.com>
2023-05-01 21:56:29 -0400

    validaciones_termidadas_yeaaaaa

--- vali_index.js
+++ vali_index.js
@@ -6,6 +6,13 @@ $(document).ready(function () {
         }
     });
 
+    // Bloquear el uso de la tecla espacio en los inputs de tipo password
+    $('input[type="password"]').on('keydown', function (e) {
+        if (e.keyCode === 32) {
+            return false;
+        }
+    });
+
     // Validar el correo electrónico en el campo de inicio de sesión
     $('#login-email').on('input', function () {
         var email = $(this).val();
@@ -16,6 +23,8 @@ $(document).ready(function () {
         }
     });
 
+
+    
     // Validar el correo electrónico en el campo de registro
     $('#signup-email').on('input', function () {
         var email = $(this).val();
@@ -29,7 +38,7 @@ $(document).ready(function () {
 
     // Función para validar el correo electrónico
     function isValidEmail(email) {
-        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}\.[^\s@]{1,}$/;
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+\.[^\s@]{2,}$/;
         var emailParts = email.split('@');
         var localPart = emailParts[0];
         var domainPart = emailParts[1];
@@ -39,10 +48,13 @@ $(document).ready(function () {
             localPart.length <= 64 &&
             domainPart.length >= 4 && domainPart.length <= 255 &&
             email.length <= 256 &&
-            !/ñ/.test(email) && !/Ñ/.test(email) && !/ñ/.test(domainPart) && !/Ñ/.test(domainPart) &&
             /^[a-zA-Z0-9._%+-]+$/.test(localPart) && /^[a-zA-Z0-9-]+$/.test(domainParts[0]) &&
             /^[a-zA-Z]+$/.test(topLevelDomain);
     }
+
+
+
 });
+
 
 
